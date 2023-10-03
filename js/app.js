94d7138768d355898f06d042067347b24cee2909
munim <muhammadmunimoff330@gmail.com>
2023-10-03 11:41:10 +0500

    ui with functionalities

--- js/app.js
+++ js/app.js
@@ -60,10 +60,86 @@ class UI {
       this.balance.classList.add("showPurple");
     }
   }
+
+  // Submit Expense Form
+
+  submitExpenseForm() {
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if (expenseValue === "" || amountValue === "" || amountValue < 0) {
+      this.expenseFeedback.classList.add("showItem")
+      this.expenseFeedback.innerHTML = `<p>Values Cannot be Empty or Negative</p>`
+
+      const self = this;
+      setTimeout(() => {
+        self.expenseFeedback.classList.remove("showItem")
+      }, 3000);
+    }
+    else {
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = ""
+      this.amountInput.value = ""
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount,
+      }
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+
+      // Show balance 
+    }
+  }
+  // add expense 
+  addExpense(expense) {
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `
+    <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+    `;
+    this.expenseList.appendChild(div);
+  }
   // Total Expense
   totalExpense() {
-    let total = 400;
+    let total = 0;
+    if (this.itemList.length > 0) {
+      total = this.itemList.reduce(function (acc, currVal) {
+        console.log(`total is ${acc} ant he current value is ${currVal}`);
+
+        acc += currVal.amount;
+
+        return acc;
+      }, 0)
+    }
+    this.expenseAmount.textContent = total;
     return total;
+  }
+
+  // editExpense
+  editExpense(element) {
+
+  }
+
+  // deleteExpense
+  deleteExpense(element) {
+
   }
 }
 
@@ -89,9 +165,13 @@ function eventListeners() {
   })
 
   // budget form submit 
-  expenseList.addEventListener("click", function () {
-
-  })
+  expenseList.addEventListener("click", function (event) {
+    if (event.target.parentElement.classList.contains("edit-icon")) {
+      ui.editExpense(event.target.parentElement)
+    } else if (event.target.parentElement.classList.contains("delete-icon")) {
+      ui.deleteExpense(event.target.parentElement)
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
